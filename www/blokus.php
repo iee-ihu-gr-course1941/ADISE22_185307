af805710bf5a44c70d5924979541d6c1b13cd3f3
NikoletaFotopoulou <nikoleta.fotopoulou@gmail.com>
2022-12-23 01:26:25 +0200

    db connection. board, db upass, blokus and gitignore update

--- www/blokus.php
+++ www/blokus.php
@@ -1,8 +1,37 @@
 <?php
+
+require_once "../lib/dbconnect.php";
+require_once "../lib/board.php";
+
+
 $method = $_SERVER['REQUEST_METHOD'];
 $request=explode('/', trim($_SERVER['PATH_INFO'],'/'));
 //$request= explode('/',trim($_SERVER['SCRIPT_NAME'],'/'));
 $input = json_decode(file_get_contents('php://input'), true);
-print_r($_REQUEST);
-exit;
+
+switch ($r=array_shift($request)){
+    case 'board' :
+        switch ($b=array_shift($request)){
+            case '':
+            case null: handle_board($method);break;
+            default: header("HTTP/1.1 404 Not Found"); break;
+            
+        }
+        break;
+        default: header("HTTP/1.1 404 Not Found");
+        exit;
+
+}
+
+function handle_board($method){
+    if($method=='GET'){
+        show_board();
+    }else if ($method=='POST'){
+        reset_board();
+    }else{
+        header('HTTP/1.1405 Method Not Allowed');
+    }
+}
+
+
 ?>
